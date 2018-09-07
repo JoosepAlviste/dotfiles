5b99c37535cec4af6e1b953453fc682ce651bc73
Joosep Alviste <joosep.alviste@gmail.com>
2018-09-07 21:20:53 +0200

    Dunst configuration

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -11,7 +11,7 @@ olddir=~/.dotfiles_old           # old dotfiles backup directory
 # list of files/folders to symlink in homedir
 files="vimrc vim zshrc gitconfig tmux.conf ctags.d ideavimrc Xresources"
 
-dotConfigFiles="nvim tmux.conf.d karabiner gitignore_global i3 rofi polybar ranger fontconfig"
+dotConfigFiles="nvim tmux.conf.d karabiner gitignore_global i3 rofi polybar ranger fontconfig dunst"
 
 ##########
 
