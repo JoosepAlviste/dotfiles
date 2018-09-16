6efbce34a7b2ca0614875857fe135891b1d9d8aa
Joosep Alviste <joosep.alviste@gmail.com>
2018-09-16 13:42:01 +0200

    Some improvements to rofi bookmarks

--- scripts/rofi-scripts-collection/rofi-surfraw-bookmarks.sh
+++ scripts/rofi-scripts-collection/rofi-surfraw-bookmarks.sh
@@ -19,4 +19,4 @@
 # set your browser (uncomment if needed, some GUI does not detect browser variable)
 # BROWSER=firefox
 
-surfraw -browser=$BROWSER "$(cat ~/.config/surfraw/bookmarks/bookmarks | sed '/^$/d' | sed '/^#/d' | sed '/^\//d' | sort -n | rofi -dmenu -mesg ">>> Edit to add new bookmarks at ~/.config/surfraw/bookmarks" -i -p "rofi-surfraw-bookmarks: ")"
+surfraw -browser=$BROWSER "$(cat ~/.config/surfraw/bookmarks | sed '/^$/d' | sed '/^#/d' | sed '/^\//d' | sort -n | rofi -dmenu -mesg ">>> Edit to add new bookmarks at ~/.config/surfraw/bookmarks/bookmarks" -i -p "rofi-surfraw-bookmarks")"
