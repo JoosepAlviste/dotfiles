1839eac8c9ac16d16d4706bb7fa0ec8022f0a4f5
Joosep Alviste <joosep.alviste@gmail.com>
2018-09-23 00:54:23 +0200

    Add Rofi power control script

---
+++ scripts/rofi-scripts-collection/rofi-power.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Open a Rofi selection with various power options.
+# Needs a file containing options to choose from at $OPTIONS_FILE.
+
+OPTIONS_FILE=~/.config/scripts/rofi-scripts-collection/dmenu-i3exit
+PROMPT="Power"
+
+res=$(rofi -dmenu < $OPTIONS_FILE -p $PROMPT)
+
+if [ "$res" = "logout" ]; then
+    i3-msg exit
+fi
+if [ "$res" = "restart" ]; then
+    reboot
+fi
+if [ "$res" = "shutdown" ]; then
+    poweroff
+fi
+
+exit 0
+
