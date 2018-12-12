2d69a3fb608f4a3f315730eb4e8ca59d1d654efb
Joosep Alviste <joosep.alviste@gmail.com>
2018-12-12 20:32:26 +0100

    Remove unneeded rofi bang scripts

--- config/scripts/rofi-scripts-collection/rofi-audio-output.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-OUTPUT=$(pactl list short sinks | cut  -f 2 | rofi -dmenu -p "Output")
-pacmd set-default-sink "$OUTPUT" >/dev/null 2>&1
-
-for playing in $(pacmd list-sink-inputs | awk '$1 == "index:" {print $2}'); do
-    pacmd move-sink-input "$playing" "$OUTPUT" >/dev/null 2>&1
-done
-
