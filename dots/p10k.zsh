973445479d3b6b3e1ea1e6bb1ca0b1010f516fa9
Joosep Alviste <joosep.alviste@gmail.com>
2020-03-29 11:09:15 +0300

    Add back Pyenv info to prompt

--- dots/p10k.zsh
+++ dots/p10k.zsh
@@ -52,7 +52,7 @@
     # direnv                  # direnv status (https://direnv.net/)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     # anaconda                # conda environment (https://conda.io/)
-    # pyenv                   # python environment (https://github.com/pyenv/pyenv)
+    pyenv                   # python environment (https://github.com/pyenv/pyenv)
     goenv                   # go environment (https://github.com/syndbg/goenv)
     # nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
     # nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
