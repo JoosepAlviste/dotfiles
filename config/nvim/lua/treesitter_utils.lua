ba58e228292531f121dead52b136162271e75862
Joosep Alviste <joosep.alviste@gmail.com>
2020-11-27 10:13:52 +0200

    Vim: Add JSDoc parser for Treesitter
    Now that nested parsers work, we can automatically install the JSDoc
    parser to highlight JSDoc comments nicely.

--- config/nvim/lua/treesitter_utils.lua
+++ config/nvim/lua/treesitter_utils.lua
@@ -10,6 +10,6 @@ require'nvim-treesitter.configs'.setup {
     persist_queries = false,
   },
   ensure_installed = {
-    'query', 'javascript', 'typescript', 'tsx', 'json', 'php', 'python', 'html',
+    'query', 'javascript', 'jsdoc', 'typescript', 'tsx', 'json', 'php', 'python', 'html',
   },
 }
