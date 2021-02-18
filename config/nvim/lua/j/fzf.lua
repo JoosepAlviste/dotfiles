77f0f7a42b770830dd5d212884a1a7875e08cae5
Joosep Alviste <joosep.alviste@gmail.com>
2021-02-18 22:08:48 +0200

    Vim: Set up live grep with FZF

--- config/nvim/lua/j/fzf.lua
+++ config/nvim/lua/j/fzf.lua
@@ -6,11 +6,12 @@ local M = {}
 
 function M.setup()
   map('n', '<c-p>', [[<cmd>lua require('j.fzf').files()<cr>]])
+  map('n', '<leader>ff', [[<cmd>lua require('j.fzf').grep()<cr>]])
 end
 
 function M.files()
   local command = 'fd --color always -t f -L --hidden'
-  local preview = 'bat --line-range=:$(($FZF_PREVIEW_LINES)) --color always -- {}'
+  local preview = 'bat --color always -- {}'
 
   coroutine.wrap(function ()
     local choices = fzf(
@@ -35,6 +36,37 @@ function M.files()
 
     for i=2,#choices do
       vim.cmd(vimcmd .. ' ' .. vim.fn.fnameescape(choices[i]))
+    end
+  end)()
+end
+
+function M.grep()
+  local command = 'rg --column --line-number --no-heading --color=always --smart-case -- ""'
+  local preview = 'fzf-preview {}'
+
+  coroutine.wrap(function ()
+    local choices = fzf(
+      command,
+      ('--ansi --prompt "Grep > " --delimiter : --preview-window "+{2}-/2" --expect=ctrl-s,ctrl-t,ctrl-v --multi --preview=%s'):format(
+        vim.fn.shellescape(preview)
+      )
+    )
+
+    if not choices then return end
+
+    local vimcmd
+    if choices[1] == 'ctrl-t' then
+      vimcmd = 'tabnew'
+    elseif choices[1] == 'ctrl-v' then
+      vimcmd = 'vnew'
+    elseif choices[1] == 'ctrl-s' then
+      vimcmd = 'new'
+    else
+      vimcmd = 'e'
+    end
+
+    for i=2,#choices do
+      vim.cmd(vimcmd .. ' ' .. vim.fn.fnameescape(vim.gsplit(choices[i], ':')()))
     end
   end)()
 end
