0ad4f173ddb5695d16e4d1452a43c38879df28bc
Joosep Alviste <joosep.alviste@gmail.com>
2021-02-24 19:49:16 +0200

    Vim: Fix <cr> when pum open

--- config/nvim/lua/j/autopairs.lua
+++ config/nvim/lua/j/autopairs.lua
@@ -4,26 +4,14 @@ local map = require('j.utils').map
 
 local M = {}
 
-function MUtils.confirm()
-  if vim.fn.pumvisible() ~= 0  then
---     if vim.fn.complete_info()["selected"] ~= -1 then
---       vim.fn["compe#confirm"]()
---       return autopairs.esc("<c-y>")
---     else
---       vim.defer_fn(function()
---         vim.fn["compe#confirm"]("<cr>")
---       end, 20)
---       return autopairs.esc("<c-n>")
---     end
-  else
-    return autopairs.check_break_line_char()
-  end
+function _G.confirm()
+  return autopairs.check_break_line_char()
 end
 
 function M.setup()
   autopairs.setup()
 
-  map('i', '<cr>', 'v:lua.MUtils.confirm()', {expr = true, noremap = true})
+  map('i', '<cr>', [[v:lua.confirm()]], {expr = true, noremap = true})
 end
 
 return M
