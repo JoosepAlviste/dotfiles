6c2471277d4d81349d1e7c23115cc214e587b9e6
Joosep Alviste <joosep.alviste@gmail.com>
2021-03-21 20:10:13 +0200

    Vim: Improve compe config a bit
    Copy the defaults from their README. I'm not sure if this improves
    anything, but it's worth a shot.

--- config/nvim/lua/j/plugins/completion.lua
+++ config/nvim/lua/j/plugins/completion.lua
@@ -6,11 +6,25 @@ local M = {}
 
 function M.setup()
   compe.setup {
+    enabled = true,
+    autocomplete = true,
+    debug = false,
+    min_length = 1,
+    preselect = 'enable',
+    throttle_time = 80,
+    source_timeout = 200,
+    incomplete_delay = 400,
+    max_abbr_width = 100,
+    max_kind_width = 100,
+    max_menu_width = 100,
+    documentation = true,
+
     source = {
       path = true,
       buffer = true,
       nvim_lsp = true,
       vsnip = true,
+      calc = true,
     },
   }
 
