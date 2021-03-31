84b77ad7f3f7740a25d5e504c2a7c54f08887bf7
Joosep Alviste <joosep.alviste@gmail.com>
2021-03-31 19:15:25 +0300

    Vim: Add help tags finding with FZF

--- config/nvim/lua/j/plugins/fzf.lua
+++ config/nvim/lua/j/plugins/fzf.lua
@@ -1,3 +1,5 @@
+local runtimepaths = vim.api.nvim_list_runtime_paths()
+
 local fzf = require('fzf').fzf
 
 local utils = require('j.utils')
@@ -73,6 +75,7 @@ function M.setup()
   map('n', '<leader>fr', [[<cmd>lua require('j.plugins.fzf').history()<cr>]])
   map('n', '<leader>fx', [[<cmd>lua require('j.plugins.fzf').git_status()<cr>]])
   map('n', '<leader>fd', [[<cmd>lua require('j.plugins.fzf').directories()<cr>]])
+  map('n', '<leader>fh', [[<cmd>lua require('j.plugins.fzf').help_tags()<cr>]])
 
   create_augroups({
     fzf = {
@@ -249,6 +252,104 @@ function M.lsp_references_handler(_, _, results)
     )
 
     handle_selected_files(choices)
+  end)()
+end
+
+local function readfilecb(path, callback)
+  vim.loop.fs_open(path, "r", 438, function(err, fd)
+    if err then
+      callback(err)
+      return
+    end
+    vim.loop.fs_fstat(fd, function(err, stat)
+      if err then
+        callback(err)
+        return
+      end
+      vim.loop.fs_read(fd, stat.size, 0, function(err, data)
+        if err then
+          callback(err)
+          return
+        end
+        vim.loop.fs_close(fd, function(err)
+          if err then
+            callback(err)
+            return
+          end
+          return callback(nil, data)
+        end)
+      end)
+    end)
+  end)
+end
+
+local function readfile(name)
+  local co = coroutine.running()
+  readfilecb(name, function (err, data)
+    coroutine.resume(co, err, data)
+  end)
+  local err, data = coroutine.yield()
+  if err then error(err) end
+  return data
+end
+
+local function deal_with_tags(tagfile, cb)
+  local co = coroutine.running()
+  coroutine.wrap(function ()
+    local success, data = pcall(readfile, tagfile)
+    if success then
+      for i, line in ipairs(vim.split(data, '\n')) do
+        local items = vim.split(line, '\t')
+        -- escape codes for grey
+        local tag = string.format('%s\t\27[0;37m%s\27[0m', items[1], items[2])
+        local co = coroutine.running()
+        cb(tag, function ()
+          coroutine.resume(co)
+        end)
+        coroutine.yield()
+      end
+    end
+    coroutine.resume(co)
+  end)()
+  coroutine.yield()
+end
+
+local function fzf_function(cb)
+  local total_done = 0
+  for i, rtp in ipairs(runtimepaths) do
+    local tagfile = table.concat({rtp, 'doc', 'tags'}, '/')
+    -- wrapping to make all the file reading concurrent
+    coroutine.wrap(function ()
+      deal_with_tags(tagfile, cb)
+      total_done = total_done + 1
+      if total_done == #runtimepaths then
+        cb(nil)
+      end
+    end)()
+  end
+end
+
+function M.help_tags()
+  coroutine.wrap(function ()
+    local result = fzf(fzf_function, '--nth 1 --ansi --expect=ctrl-t,ctrl-s,ctrl-v')
+    if not result then
+      return
+    end
+    local choice = vim.split(result[2], '\t')[1]
+    local key = result[1]
+    local windowcmd
+    if key == '' or key == 'ctrl-s' then
+      windowcmd = ''
+    elseif key == 'ctrl-v' then
+      windowcmd = 'vertical'
+    elseif key == 'ctrl-t' then
+      windowcmd = 'tab'
+    else
+      print('Not implemented!')
+      error('Not implemented!')
+    end
+
+    vim.cmd(string.format('%s h %s', windowcmd, choice))
   end)()
 end
 
