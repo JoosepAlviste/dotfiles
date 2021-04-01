7e345c8ab1dd3af0258015c04d8762b408fe8207
Joosep Alviste <joosep.alviste@gmail.com>
2021-04-01 19:26:29 +0300

    Vim: Improve nvim-tree icons & mappings

--- config/nvim/lua/j/plugins/tree.lua
+++ config/nvim/lua/j/plugins/tree.lua
@@ -1,5 +1,7 @@
 local g = vim.g
 
+local tree_cb = require('nvim-tree.config').nvim_tree_callback
+
 local map = require('j.utils').map
 
 local M = {}
@@ -17,17 +19,24 @@ function M.setup()
     default = '',
     symlink = '',
     folder = {
-      default = "",
-      open = "",
-      empty = "",
-      empty_open = "",
-      symlink = "",
-      symlink_open = "",
+      default = '',
+      open = '',
+      empty = '',
+      empty_open = '',
+      symlink = '',
+      symlink_open = '',
     },
   }
+  g.nvim_tree_ignore = {'.git', '.DS_Store'}
 
   -- Mappings
   map('n', '<c-n>', ':NvimTreeToggle<cr>', {silent = true})
+
+
+  -- In the tree
+  g.nvim_tree_bindings = {
+    ['p'] = tree_cb('close_node'),
+  }
 end
 
 return M
