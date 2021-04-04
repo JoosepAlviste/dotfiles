a06684eee768b85295e1b21569f8e658dfad5962
Joosep Alviste <joosep.alviste@gmail.com>
2021-04-04 14:00:41 +0300

    Vim: Make projectionist config less verbose

--- config/nvim/lua/j/plugins/projectionist.lua
+++ config/nvim/lua/j/plugins/projectionist.lua
@@ -1,46 +1,40 @@
 local map = require('j.utils').map
 
-local M = {}
-
-function M.setup()
-  vim.g.projectionist_heuristics = {
-    ['src/*'] = {
-      ['*.ts'] = {
-        alternate = {
-          '{dirname}/{basename}.test.ts',
-          '{dirname}/__tests__/{basename}.test.ts',
-        },
-        type = 'source',
+vim.g.projectionist_heuristics = {
+  ['src/*'] = {
+    ['*.ts'] = {
+      alternate = {
+        '{dirname}/{basename}.test.ts',
+        '{dirname}/__tests__/{basename}.test.ts',
       },
-      ['*.tsx'] = {
-        alternate = {
-          '{dirname}/{basename}.test.ts',
-          '{dirname}/{basename}.test.tsx',
-          '{dirname}/__tests__/{basename}.test.ts',
-          '{dirname}/__tests__/{basename}.test.tsx',
-        },
-        type = 'source',
-      },
-      ['*.test.ts'] = {
-        alternate = {
-          '{dirname}/{basename}.ts',
-          '{dirname}/../{basename}.ts',
-        },
-        type = 'test',
-      },
-      ['*.test.tsx'] = {
-        alternate = {
-          '{dirname}/{basename}.tsx',
-          '{dirname}/../{basename}.tsx',
-        },
-        type = 'test',
-      },
+      type = 'source',
     },
-  }
+    ['*.tsx'] = {
+      alternate = {
+        '{dirname}/{basename}.test.ts',
+        '{dirname}/{basename}.test.tsx',
+        '{dirname}/__tests__/{basename}.test.ts',
+        '{dirname}/__tests__/{basename}.test.tsx',
+      },
+      type = 'source',
+    },
+    ['*.test.ts'] = {
+      alternate = {
+        '{dirname}/{basename}.ts',
+        '{dirname}/../{basename}.ts',
+      },
+      type = 'test',
+    },
+    ['*.test.tsx'] = {
+      alternate = {
+        '{dirname}/{basename}.tsx',
+        '{dirname}/../{basename}.tsx',
+      },
+      type = 'test',
+    },
+  },
+}
 
-  map('n', '<leader>av', [[:AV<cr>]])
-  map('n', '<leader>as', [[:AS<cr>]])
-  map('n', '<leader>ae', [[:A<cr>]])
-end
-
-return M
+map('n', '<leader>av', [[:AV<cr>]])
+map('n', '<leader>as', [[:AS<cr>]])
+map('n', '<leader>ae', [[:A<cr>]])
