41fdef8978ddbf482bc2f61f5849579f289f389b
Joosep Alviste <joosep.alviste@gmail.com>
2021-04-04 14:01:21 +0300

    Vim: Make nvim-tree config less verbose

--- config/nvim/lua/j/plugins/tree.lua
+++ config/nvim/lua/j/plugins/tree.lua
@@ -2,33 +2,27 @@ local g = vim.g
 
 local map = require('j.utils').map
 
-local M = {}
+-- Settings
+g.nvim_tree_git_hl = 1
+g.nvim_tree_auto_close = 1
+g.nvim_tree_show_icons = {
+  git = 0,
+  folders = 1,
+  files = 1,
+}
+g.nvim_tree_icons = {
+  default = '',
+  symlink = '',
+  folder = {
+    default = '',
+    open = '',
+    empty = '',
+    empty_open = '',
+    symlink = '',
+    symlink_open = '',
+  },
+}
+g.nvim_tree_ignore = {'.git', '.DS_Store'}
 
-function M.setup()
-  -- Settings
-  g.nvim_tree_git_hl = 1
-  g.nvim_tree_auto_close = 1
-  g.nvim_tree_show_icons = {
-    git = 0,
-    folders = 1,
-    files = 1,
-  }
-  g.nvim_tree_icons = {
-    default = '',
-    symlink = '',
-    folder = {
-      default = '',
-      open = '',
-      empty = '',
-      empty_open = '',
-      symlink = '',
-      symlink_open = '',
-    },
-  }
-  g.nvim_tree_ignore = {'.git', '.DS_Store'}
-
-  -- Mappings
-  map('n', '<c-n>', ':NvimTreeToggle<cr>', {silent = true})
-end
-
-return M
+-- Mappings
+map('n', '<c-n>', ':NvimTreeToggle<cr>', {silent = true})
