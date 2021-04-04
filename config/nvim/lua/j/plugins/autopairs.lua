312be8ed0f16d8b3d6632bde4cc746bf1b07d218
Joosep Alviste <joosep.alviste@gmail.com>
2021-04-04 13:32:46 +0300

    Vim: Make autopairs configuration less verbose

--- config/nvim/lua/j/plugins/autopairs.lua
+++ config/nvim/lua/j/plugins/autopairs.lua
@@ -2,18 +2,12 @@ local autopairs = require('nvim-autopairs')
 
 local map = require('j.utils').map
 
-local M = {}
-
 function _G.confirm()
   return autopairs.check_break_line_char()
 end
 
-function M.setup()
-  autopairs.setup({
-    check_line_pair = false,
-  })
+autopairs.setup({
+  check_line_pair = false,
+})
 
-  map('i', '<cr>', [[v:lua.confirm()]], {expr = true, noremap = true})
-end
-
-return M
+map('i', '<cr>', [[v:lua.confirm()]], {expr = true, noremap = true})
