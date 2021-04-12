6cba0d7728f6da1e5915d2e5834deccb41727f4d
Joosep Alviste <joosep.alviste@gmail.com>
2021-04-12 20:07:58 +0300

    Vim: Do not show error in expand if comma exists

--- config/nvim/lua/j/expand.lua
+++ config/nvim/lua/j/expand.lua
@@ -4,7 +4,7 @@ local M = {}
 -- current line. Mainly meant to be used in JSON files.
 function M.insert_trailing_comma()
   -- Add the trailing comma if it doesn't exist
-  vim.cmd [[keeppatterns substitute/\(.*[^{[][^,{[]\)$/\1,/g]]
+  vim.cmd [[silent! keeppatterns substitute/\(.*[^{[][^,{[]\)$/\1,/g]]
   -- And add the new line
   vim.fn.feedkeys(vim.api.nvim_replace_termcodes('A<cr>', true, true, true))
 end
