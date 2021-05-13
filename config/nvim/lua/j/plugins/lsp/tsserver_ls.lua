88f16a960252000cc2d8c3814c5d1c62a7d137bd
Joosep Alviste <joosep.alviste@gmail.com>
2021-05-13 10:28:42 +0300

    Vim: Fix tsserver setup
    Oops

--- config/nvim/lua/j/plugins/lsp/tsserver_ls.lua
+++ config/nvim/lua/j/plugins/lsp/tsserver_ls.lua
@@ -1,11 +1,11 @@
 -- https://github.com/theia-ide/typescript-language-server
--- require('lspconfig').tsserver.setup{
---   on_attach = function(client, bufnr)
---     -- Disable the document formatting for tsserver because we want to use efm 
---     -- with ESLint
---     client.resolved_capabilities.document_formatting = false
+require('lspconfig').tsserver.setup{
+  on_attach = function(client, bufnr)
+    -- Disable the document formatting for tsserver because we want to use efm 
+    -- with ESLint
+    client.resolved_capabilities.document_formatting = false
 
---     require('j.plugins.lsp').on_attach(client, bufnr)
---   end,
---   capabilities = require('j.plugins.lsp').capabilities,
--- }
+    require('j.plugins.lsp').on_attach(client, bufnr)
+  end,
+  capabilities = require('j.plugins.lsp').capabilities,
+}
