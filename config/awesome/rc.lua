4ec18c2cc5b4288d532c0d843477c720abc76488
Joosep Alviste <joosep.alviste@gmail.com>
2021-06-14 22:39:56 +0300

    Add default AwesomeWM theme

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -45,7 +45,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+local theme_path = string.format('%s/.config/awesome/themes/%s/theme.lua', os.getenv('HOME'), 'default')
+beautiful.init(theme_path)
 
 -- This is used later as the default terminal and editor to run.
 terminal = os.getenv("TERMINAL")
