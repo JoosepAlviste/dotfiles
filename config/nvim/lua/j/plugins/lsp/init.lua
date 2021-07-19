302c077612406bcaff61d04a97e66bf5d30ae005
Joosep Alviste <joosep.alviste@gmail.com>
2021-07-19 10:04:03 +0300

    Vim: Disable LSP diagnostics virtual text

--- config/nvim/lua/j/plugins/lsp/init.lua
+++ config/nvim/lua/j/plugins/lsp/init.lua
@@ -14,11 +14,7 @@ vim.fn.sign_define('LspDiagnosticsSignHint', {text = ''})
 -- Configure diagnostics displaying
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = {
-      prefix = '»',
-      spacing = 4,
-      severity_limit = 'Warning',
-    },
+    virtual_text = false,
     signs = true,
     update_in_insert = false,
   }
