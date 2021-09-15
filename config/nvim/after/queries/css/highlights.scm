15983e035ec40673f202b898ce085a4ceadff27e
Joosep Alviste <joosep.alviste@gmail.com>
2021-09-15 10:06:16 +0300

    Vim: Remove unneeded treesitter highlights
    These were moved into the colorscheme.

--- config/nvim/after/queries/css/highlights.scm
+++
@@ -1,5 +0,0 @@
-(class_name) @constructor
-(tag_name) @variable.builtin
-
-(plain_value) @variable.builtin
-(unit) @variable.builtin
--- config/nvim/after/queries/yaml/highlights.scm
+++
@@ -1,6 +0,0 @@
-(block_mapping_pair key: (_) @function)
-
-(block_mapping_pair value:
-  (flow_node
-    (plain_scalar
-      (string_scalar) @string)))
