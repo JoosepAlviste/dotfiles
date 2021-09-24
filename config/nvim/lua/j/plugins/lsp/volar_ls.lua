5c83898c2767d432c761f16d9af948e25f5785be
Joosep Alviste <joosep.alviste@gmail.com>
2021-09-24 12:28:59 +0300

    Vim: Add Volar LSP server

---
+++ config/nvim/lua/j/plugins/lsp/volar_ls.lua
@@ -0,0 +1,11 @@
+-- https://github.com/johnsoncodehk/volar
+require'lspconfig'.volar.setup{
+  on_attach = function(client, bufnr)
+    -- Disable the document formatting for vuels because we want to use efm 
+    -- with ESLint
+    client.resolved_capabilities.document_formatting = false
+
+    require('j.plugins.lsp').on_attach(client, bufnr)
+  end,
+  capabilities = require('j.plugins.lsp').capabilities,
+}
--- config/nvim/lua/j/plugins/lsp/vue_ls.lua
+++ config/nvim/lua/j/plugins/lsp/vue_ls.lua
@@ -1,29 +1,29 @@
 -- https://github.com/vuejs/vetur/tree/master/server
-require('lspconfig').vuels.setup{
-  on_attach = function(client, bufnr)
-    -- Disable the document formatting for vuels because we want to use efm 
-    -- with ESLint
-    client.resolved_capabilities.document_formatting = false
+-- require('lspconfig').vuels.setup{
+--   on_attach = function(client, bufnr)
+--     -- Disable the document formatting for vuels because we want to use efm 
+--     -- with ESLint
+--     client.resolved_capabilities.document_formatting = false
 
-    require('j.plugins.lsp').on_attach(client, bufnr)
-  end,
-  capabilities = require('j.plugins.lsp').capabilities,
-  settings = {
-    vetur = {
-      experimental = {
-        templateInterpolationService = true,
-      },
-      validation = {
-        templateProps = true,
-      },
-      completion = {
-        tagCasing = 'initial',
-        autoImport = true,
-        useScaffoldSnippets = true,
-      },
-    },
-  },
-  flags = {
-    debounce_text_changes = 150,
-  },
-}
+--     require('j.plugins.lsp').on_attach(client, bufnr)
+--   end,
+--   capabilities = require('j.plugins.lsp').capabilities,
+--   settings = {
+--     vetur = {
+--       experimental = {
+--         templateInterpolationService = true,
+--       },
+--       validation = {
+--         templateProps = true,
+--       },
+--       completion = {
+--         tagCasing = 'initial',
+--         autoImport = true,
+--         useScaffoldSnippets = true,
+--       },
+--     },
+--   },
+--   flags = {
+--     debounce_text_changes = 150,
+--   },
+-- }
