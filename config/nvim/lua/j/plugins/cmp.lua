b6df3bbc7d261cb9dd439ed234e3e46a27416b67
Joosep Alviste <joosep.alviste@gmail.com>
2021-10-09 23:25:10 +0300

    Vim: Re-order completion sources

--- config/nvim/lua/j/plugins/cmp.lua
+++ config/nvim/lua/j/plugins/cmp.lua
@@ -20,9 +20,9 @@ cmp.setup {
 
   sources = {
     { name = 'nvim_lsp' },
-    { name = 'buffer' },
-    { name = 'path' },
     { name = 'vsnip' },
+    { name = 'path' },
+    { name = 'buffer' },
   },
 
   documentation = {
