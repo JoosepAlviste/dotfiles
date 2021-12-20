8a2e1486d3a88532571cd76d52457fac6856e9f4
Joosep Alviste <joosep.alviste@gmail.com>
2021-12-20 20:22:08 +0200

    Vim: Make GraphQL LS only active in GraphQL files

--- config/nvim/lua/j/plugins/lsp/graphql_ls.lua
+++ config/nvim/lua/j/plugins/lsp/graphql_ls.lua
@@ -5,6 +5,7 @@ require('lspconfig').graphql.setup {
   on_attach = require('j.plugins.lsp').on_attach,
   capabilities = require('j.plugins.lsp').capabilities,
   root_dir = util.root_pattern('.graphqlrc.yml', '.graphqlrc'),
+  filetypes = { 'graphql' },
   flags = {
     debounce_text_changes = 150,
   },
