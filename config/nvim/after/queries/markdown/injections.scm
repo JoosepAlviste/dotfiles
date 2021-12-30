8c38281a8fb45300a1c3ba102a9e281cc1b759be
Joosep Alviste <joosep.alviste@gmail.com>
2021-12-30 15:35:28 +0200

    Vim: Inject JS into Markdown with Treesitter

--- config/nvim/after/queries/markdown/injections.scm
+++ config/nvim/after/queries/markdown/injections.scm
@@ -1,3 +1,7 @@
 (fenced_code_block
   (info_string) @_lang (#eq? @_lang "sh")
   (code_fence_content) @bash)
+
+(fenced_code_block
+  (info_string) @_lang (#eq? @_lang "js")
+  (code_fence_content) @javascript)
