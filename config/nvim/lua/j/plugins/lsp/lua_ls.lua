1e9ae41823892cd4bb2a4ca9b0937b08712a1e62
Joosep Alviste <joosep.alviste@gmail.com>
2022-01-17 18:56:02 +0200

    Vim: Remove path configuration for Sumneko Lua LSP
    It is now possible to install the server with:
    
    ```
    brew install lua-language-server
    ```

--- config/nvim/lua/j/plugins/lsp/lua_ls.lua
+++ config/nvim/lua/j/plugins/lsp/lua_ls.lua
@@ -5,7 +5,6 @@ local luadev = require('lua-dev').setup {
   lspconfig = {
     on_attach = require('j.plugins.lsp').on_attach,
     capabilities = require('j.plugins.lsp').capabilities,
-    cmd = { 'lua-language-server', '-E', vim.fn.expand '$HOME/Code/Programs/lua-language-server/main.lua' },
     flags = {
       debounce_text_changes = 150,
     },
