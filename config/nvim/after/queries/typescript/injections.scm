01fcbaa4345ceb5c5125226dceb1c495e06bb086
Joosep Alviste <joosep.alviste@gmail.com>
2022-01-27 22:30:17 +0200

    Vim: Inject GraphQL into graphql-request calls

--- config/nvim/after/queries/typescript/injections.scm
+++ config/nvim/after/queries/typescript/injections.scm
@@ -7,3 +7,10 @@
 (pair
   key: (property_identifier) @_key (#eq? @_key "template")
   value: (template_string) @vue)
+
+(call_expression
+  function: (await_expression
+    (member_expression) @_name
+      (#lua-match? @_name "request$"))
+  arguments: (arguments
+    (template_string) @graphql))
