2bffc0e43ed9f5de6f1ca9dcb03d3e13ac0bae8c
Joosep Alviste <joosep.alviste@gmail.com>
2022-02-16 19:14:46 +0200

    Vim: Enable ESLint when configuration in .cjs file
    This is probably necessary when `type = module` in `package.json` so
    that ES modules are used.

--- config/nvim/lua/j/plugins/lsp/eslint_ls.lua
+++ config/nvim/lua/j/plugins/lsp/eslint_ls.lua
@@ -6,5 +6,5 @@ require('lspconfig').eslint.setup {
 
     require('j.plugins.lsp').on_attach(client, bufnr)
   end,
-  root_dir = util.root_pattern '.eslintrc.js',
+  root_dir = util.root_pattern('.eslintrc.js', '.eslintrc.cjs'),
 }
