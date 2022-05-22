5f15446868a95459406f81d9461a1d509b1d32df
Joosep Alviste <joosep.alviste@gmail.com>
2022-05-22 19:11:45 +0300

    Vim: Add snippets for JS tests

--- config/nvim/lua/j/plugins/snippets/javascript.lua
+++ config/nvim/lua/j/plugins/snippets/javascript.lua
@@ -1,8 +1,12 @@
 local ls = require 'luasnip'
 
-local s = ls.s
 local fmt = require('luasnip.extras.fmt').fmt
+local s = ls.snippet
 local i = ls.insert_node
+local d = ls.dynamic_node
+local sn = ls.snippet_node
+local c = ls.choice_node
+local t = ls.text_node
 
 ls.add_snippets('javascript', {
   s(
@@ -36,6 +40,43 @@ if ({}) {{
 }}
 ]],
       { i(1), i(2) }
+    )
+  ),
+  s(
+    'desc',
+    fmt(
+      [[
+describe('{}', () => {{
+  {}
+}});
+]],
+      {
+        d(1, function(_, snip)
+          P(snip.env.TM_DIRECTORY:gsub(vim.pesc(vim.loop.cwd()), '.'))
+          local filename, _ = (snip.env.TM_DIRECTORY .. '/' .. snip.env.TM_FILENAME_BASE):gsub(
+            vim.pesc(vim.loop.cwd() .. '/'),
+            ''
+          )
+          local filename_without_test, _ = filename:gsub('^test/', '')
+          local filename_without_extension, _ = filename_without_test:gsub('.spec$', '')
+
+          return sn(nil, {
+            i(1, filename_without_extension),
+          })
+        end, { 1 }),
+        i(2),
+      }
+    )
+  ),
+  s(
+    'it',
+    fmt(
+      [[
+it('{}', {}() => {{
+  {}
+}});
+]],
+      { i(1), c(2, { t 'async ', t '' }), i(3) }
     )
   ),
 })
