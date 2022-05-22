71cb7bd031e07e85a44d9802435a997e280cad1c
Joosep Alviste <joosep.alviste@gmail.com>
2022-05-22 19:54:40 +0300

    Vim: Improve Lua require snippet

--- config/nvim/lua/j/plugins/snippets/lua.lua
+++ config/nvim/lua/j/plugins/snippets/lua.lua
@@ -3,14 +3,17 @@ local ls = require 'luasnip'
 local s = ls.s
 local fmt = require('luasnip.extras.fmt').fmt
 local i = ls.insert_node
-local rep = require('luasnip.extras').rep
+local f = ls.function_node
 
 ls.add_snippets('lua', {
   s(
     'req',
-    fmt("local {} = require('{}')", {
+    fmt([[local {} = require('{}')]], {
+      f(function(import_name)
+        local parts = vim.split(import_name[1][1], '.', true)
+        return parts[#parts] or ''
+      end, { 1 }),
       i(1),
-      rep(1),
     })
   ),
 })
