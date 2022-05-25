73d14410f17e02fef3f2d3fb0a1079755a836fa8
Joosep Alviste <joosep.alviste@gmail.com>
2022-05-25 22:41:47 +0300

    Vim: Do not show LSP hover message notification

--- config/nvim/lua/j/plugins/notify.lua
+++ config/nvim/lua/j/plugins/notify.lua
@@ -8,9 +8,12 @@ vim.notify = require 'notify'
 
 local notify = vim.notify
 vim.notify = function(msg, ...)
-  if type(msg) == 'string' and msg:match '%[lspconfig] Autostart for' then
-    -- Do not show LSP autostart failed messages
-    return
+  if type(msg) == 'string' then
+    local is_suppressed_message = msg:match '%[lspconfig] Autostart for' or msg:match 'No information available'
+    if is_suppressed_message then
+      -- Do not show some messages
+      return
+    end
   end
 
   notify(msg, ...)
