e78811dd893e1160d56b6057b94fe95d7614500b
Joosep Alviste <joosep.alviste@gmail.com>
2022-07-07 15:41:05 +0300

    Vim: Ignore projectionist config file in Git

--- config/nvim/lua/j/plugins/projectionist.lua
+++ config/nvim/lua/j/plugins/projectionist.lua
@@ -1,40 +1,5 @@
 local map = require('j.utils').map
 
-vim.g.projectionist_heuristics = {
-  ['src/*'] = {
-    ['*.ts'] = {
-      alternate = {
-        '{dirname}/{basename}.test.ts',
-        '{dirname}/__tests__/{basename}.test.ts',
-      },
-      type = 'source',
-    },
-    ['*.tsx'] = {
-      alternate = {
-        '{dirname}/{basename}.test.ts',
-        '{dirname}/{basename}.test.tsx',
-        '{dirname}/__tests__/{basename}.test.ts',
-        '{dirname}/__tests__/{basename}.test.tsx',
-      },
-      type = 'source',
-    },
-    ['*.test.ts'] = {
-      alternate = {
-        '{dirname}/{basename}.ts',
-        '{dirname}/../{basename}.ts',
-      },
-      type = 'test',
-    },
-    ['*.test.tsx'] = {
-      alternate = {
-        '{dirname}/{basename}.tsx',
-        '{dirname}/../{basename}.tsx',
-      },
-      type = 'test',
-    },
-  },
-}
-
 map('n', '<leader>av', [[:AV<cr>]], { silent = true })
 map('n', '<leader>as', [[:AS<cr>]], { silent = true })
 map('n', '<leader>ae', [[:A<cr>]], { silent = true })
