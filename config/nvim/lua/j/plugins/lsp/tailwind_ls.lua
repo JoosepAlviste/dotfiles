a8c3c186585ddaf32fb9c0736df2dfc4e2b95149
Joosep Alviste <joosep.alviste@gmail.com>
2022-08-06 23:46:28 +0300

    Vim: Only enable tailwind LSP when using it
    Or something similar, like windicss.

--- config/nvim/lua/j/plugins/lsp/tailwind_ls.lua
+++ config/nvim/lua/j/plugins/lsp/tailwind_ls.lua
@@ -1,4 +1,12 @@
-require('lspconfig').tailwindcss.setup {
-  on_attach = require('j.plugins.lsp').on_attach,
-  capabilities = require('j.plugins.lsp').capabilities,
-}
+local read_package_json = require('j.utils').read_package_json
+
+local package_json = read_package_json()
+
+local have_tailwindish_styling = package_json and package_json.devDependencies.windicss
+
+if have_tailwindish_styling then
+  require('lspconfig').tailwindcss.setup {
+    on_attach = require('j.plugins.lsp').on_attach,
+    capabilities = require('j.plugins.lsp').capabilities,
+  }
+end
