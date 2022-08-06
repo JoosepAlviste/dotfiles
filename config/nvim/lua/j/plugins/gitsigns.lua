1f0b734310bd3beb9556947be270897ae3dd5334
Joosep Alviste <joosep.alviste@gmail.com>
2022-08-06 23:25:49 +0300

    Vim: Use Lua callback for navigating hunks
    The old method was not working for me for some reason.

--- config/nvim/lua/j/plugins/gitsigns.lua
+++ config/nvim/lua/j/plugins/gitsigns.lua
@@ -18,8 +18,24 @@ require('gitsigns').setup {
     end
 
     -- Navigation
-    map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-    map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+    map('n', ']c', function()
+      if vim.wo.diff then
+        return ']c'
+      end
+      vim.schedule(function()
+        gs.next_hunk()
+      end)
+      return '<Ignore>'
+    end, { expr = true })
+    map('n', '[c', function()
+      if vim.wo.diff then
+        return '[c'
+      end
+      vim.schedule(function()
+        gs.prev_hunk()
+      end)
+      return '<Ignore>'
+    end, { expr = true })
 
     -- Actions
     map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk)
