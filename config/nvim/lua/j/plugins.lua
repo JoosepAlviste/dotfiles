52dfb2dca69ef6abb146a7b514b6e18598fac511
Joosep Alviste <joosep.alviste@gmail.com>
2022-08-16 22:15:28 +0300

    Vim: Use new Comment.nvim enum values for commentstring integration

--- config/nvim/lua/j/plugins.lua
+++ config/nvim/lua/j/plugins.lua
@@ -60,14 +60,14 @@ return require('packer').startup {
             local U = require 'Comment.utils'
 
             local location = nil
-            if ctx.ctype == U.ctype.block then
+            if ctx.ctype == U.ctype.blockwise then
               location = require('ts_context_commentstring.utils').get_cursor_location()
             elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
               location = require('ts_context_commentstring.utils').get_visual_start_location()
             end
 
             return require('ts_context_commentstring.internal').calculate_commentstring {
-              key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
+              key = ctx.ctype == U.ctype.linewise and '__default' or '__multiline',
               location = location,
             }
           end,
