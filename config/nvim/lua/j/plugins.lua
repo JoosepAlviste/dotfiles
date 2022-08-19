19f79c652e14ccbed426d21eb34519c0cb8adc58
Joosep Alviste <joosep.alviste@gmail.com>
2022-08-19 20:21:37 +0300

    Vim: Use new Comment.nvim and context commentstring integration

--- config/nvim/lua/j/plugins.lua
+++ config/nvim/lua/j/plugins.lua
@@ -68,21 +68,7 @@ return require('packer').startup {
       'numToStr/Comment.nvim',
       config = function()
         require('Comment').setup {
-          pre_hook = function(ctx)
-            local U = require 'Comment.utils'
-
-            local location = nil
-            if ctx.ctype == U.ctype.blockwise then
-              location = require('ts_context_commentstring.utils').get_cursor_location()
-            elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-              location = require('ts_context_commentstring.utils').get_visual_start_location()
-            end
-
-            return require('ts_context_commentstring.internal').calculate_commentstring {
-              key = ctx.ctype == U.ctype.linewise and '__default' or '__multiline',
-              location = location,
-            }
-          end,
+          pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
         }
       end,
     }
