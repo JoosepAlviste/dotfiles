4b71a5c4caa5d542423f081d5126c4665ab687a8
Joosep Alviste <joosep.alviste@gmail.com>
2022-10-25 20:41:24 +0300

    Vim: Do not warn in CSS about Tailwind's @apply rule

--- config/nvim/lua/j/plugins/lsp/css_ls.lua
+++ config/nvim/lua/j/plugins/lsp/css_ls.lua
@@ -2,4 +2,12 @@
 require('lspconfig').cssls.setup {
   on_attach = require('j.plugins.lsp').on_attach,
   capabilities = require('j.plugins.lsp').capabilities,
+  settings = {
+    css = {
+      lint = {
+        -- Do not warn for Tailwind's @apply rule
+        unknownAtRules = 'ignore',
+      },
+    },
+  },
 }
