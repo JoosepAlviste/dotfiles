2e67d45a6ffe0d18ce983734f751f958c9800907
Joosep Alviste <joosep.alviste@gmail.com>
2022-12-13 22:44:23 +0200

    Vim: Detect more ESLint config files for ESLint LSP

--- config/nvim/lua/j/plugins/lsp/eslint_ls.lua
+++ config/nvim/lua/j/plugins/lsp/eslint_ls.lua
@@ -6,5 +6,5 @@ require('lspconfig').eslint.setup {
 
     require('j.plugins.lsp').on_attach(client, bufnr)
   end,
-  root_dir = util.root_pattern('.eslintrc.js', '.eslintrc.cjs', '.eslintrc.json'),
+  root_dir = util.root_pattern('.eslintrc.js', '.eslintrc.cjs', '.eslintrc.json', '.eslintrc'),
 }
