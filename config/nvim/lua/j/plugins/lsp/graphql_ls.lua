6d36d9db36cc7bc6b0f47d9cc25f4d0a83bdaab5
Joosep Alviste <joosep.alviste@gmail.com>
2022-12-13 22:43:58 +0200

    Vim: Enable GraphQL LSP in tsx files

--- config/nvim/lua/j/plugins/lsp/graphql_ls.lua
+++ config/nvim/lua/j/plugins/lsp/graphql_ls.lua
@@ -5,5 +5,5 @@ require('lspconfig').graphql.setup {
   on_attach = require('j.plugins.lsp').on_attach,
   capabilities = require('j.plugins.lsp').capabilities,
   root_dir = util.root_pattern('.graphqlrc.yml', '.graphqlrc'),
-  filetypes = { 'graphql' },
+  filetypes = { 'graphql', 'typescriptreact' },
 }
