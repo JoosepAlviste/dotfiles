9d5e686f497b08af98bd9579ec0c9f31fc48837c
Joosep Alviste <joosep.alviste@gmail.com>
2023-01-21 18:27:59 +0200

    Vim: Try to have fewer errors in <esc> mapping

--- config/nvim/lua/j/mappings.lua
+++ config/nvim/lua/j/mappings.lua
@@ -58,8 +58,8 @@ map('n', '<esc>', function()
   vim.cmd.nohlsearch()
 
   for _, win in ipairs(vim.api.nvim_list_wins()) do
-    local config = vim.api.nvim_win_get_config(win)
-    if config.relative ~= '' then
+    local status, config = pcall(vim.api.nvim_win_get_config, win)
+    if config and config.relative ~= '' then
       vim.api.nvim_win_close(win, false)
     end
   end
