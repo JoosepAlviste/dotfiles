a1f86006f3789fcdea161c2703a6ddb24a880a2d
Joosep Alviste <joosep.alviste@gmail.com>
2023-02-12 19:46:10 +0200

    Vim: Inline function for adding a trailing comma
    I don't use it in other places so it makes sense to keep it where it's
    used.

--- config/nvim/ftplugin/json.lua
+++ config/nvim/ftplugin/json.lua
@@ -1,14 +1,10 @@
 local termcode = require('j.utils').termcode
 
-local M = {}
-
 -- When creating a new line with o, make sure there is a trailing comma on the
--- current line. Mainly meant to be used in JSON files.
-function M.insert_trailing_comma()
+-- current line
+vim.keymap.set('n', 'o', function()
   -- Add the trailing comma if it doesn't exist
   vim.cmd [[silent! keeppatterns substitute/\(.*[^{[][^,{[]\)$/\1,/g]]
   -- And add the new line
   vim.fn.feedkeys(termcode 'A<cr>')
-end
-
-return M
+end)
--- config/nvim/ftplugin/json.vim
+++
@@ -1,1 +0,0 @@
-nnoremap <silent><buffer> o <cmd>lua require('j.expand').insert_trailing_comma()<cr>
