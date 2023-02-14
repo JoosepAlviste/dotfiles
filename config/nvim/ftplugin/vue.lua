3ed65e2d464b28263a6efe4c70b8ce7c783acc93
Joosep Alviste <joosep.alviste@gmail.com>
2023-02-14 08:24:14 +0200

    Vim: Add docs to Vue attribute qoutes mapping

--- config/nvim/ftplugin/vue.lua
+++ config/nvim/ftplugin/vue.lua
@@ -51,8 +51,11 @@ vim.keymap.set('i', '=', function()
   local cursor = vim.api.nvim_win_get_cursor(0)
   local left_of_cursor_range = { cursor[1] - 1, cursor[2] - 1 }
 
+  -- The cursor location does not give us the correct node in this case, so we
+  -- need to get the node to the left of the cursor
   local node = ts_utils.get_node_at_location(left_of_cursor_range)
-  if not node or not vim.tbl_contains({ 'attribute_name', 'directive_argument', 'directive_name' }, node:type()) then
+  local nodes_active_in = { 'attribute_name', 'directive_argument', 'directive_name' }
+  if not node or not vim.tbl_contains(nodes_active_in, node:type()) then
     return '='
   end
 
