c171efbbbe0daa5e737250ec82338a51ed53c15a
Joosep Alviste <joosep.alviste@gmail.com>
2023-03-14 16:26:41 +0200

    Vim: Simplify JSON automatic trailing comma mapping

--- config/nvim/ftplugin/json.lua
+++ config/nvim/ftplugin/json.lua
@@ -1,10 +1,12 @@
-local termcode = require('j.utils').termcode
-
 -- When creating a new line with o, make sure there is a trailing comma on the
 -- current line
 vim.keymap.set('n', 'o', function()
-  -- Add the trailing comma if it doesn't exist
-  vim.cmd [[silent! keeppatterns substitute/\(.*[^{[][^,{[]\)$/\1,/g]]
-  -- And add the new line
-  vim.fn.feedkeys(termcode 'A<cr>')
-end, { buffer = true })
+  local line = vim.api.nvim_get_current_line()
+
+  local should_add_comma = string.find(line, '[^,{[]$')
+  if should_add_comma then
+    return 'A,<cr>'
+  else
+    return 'o'
+  end
+end, { buffer = true, expr = true })
