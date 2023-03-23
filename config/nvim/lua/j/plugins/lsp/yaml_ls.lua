995aef485c3ce4102451e76d9498fd4a4a38b6ef
Joosep Alviste <joosep.alviste@gmail.com>
2023-03-23 08:33:55 +0200

    Vim: Add GitHub Actions YAML schema

--- config/nvim/lua/j/plugins/lsp/yaml_ls.lua
+++ config/nvim/lua/j/plugins/lsp/yaml_ls.lua
@@ -9,6 +9,7 @@ require('lspconfig').yamlls.setup {
         ['http://json.schemastore.org/composer'] = 'composer.yaml',
         ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = 'docker-compose*.yml',
         ['https://raw.githubusercontent.com/kamilkisiela/graphql-config/v3.0.3/config-schema.json'] = '.graphqlrc*',
+        ['https://json.schemastore.org/github-workflow.json'] = '.github/workflow/*.yml',
       },
     },
   },
