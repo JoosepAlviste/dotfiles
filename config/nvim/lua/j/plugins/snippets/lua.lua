f087ac7252769fddeca4925bd76941b2490c8cb7
Joosep Alviste <joosep.alviste@gmail.com>
2023-04-11 15:30:57 +0300

    Vim: Add condition for Lua if snippet

--- config/nvim/lua/j/plugins/snippets/lua.lua
+++ config/nvim/lua/j/plugins/snippets/lua.lua
@@ -48,7 +48,26 @@ end
 ls.add_snippets('lua', {
   -- If statement
   s(
-    'if',
+    {
+      trig = 'if',
+      condition = function()
+        local ignored_nodes = { 'string', 'comment' }
+
+        local pos = vim.api.nvim_win_get_cursor(0)
+        -- Use one column to the left of the cursor to avoid a "chunk" node
+        -- type. Not sure what it is, but it seems to be at the end of lines in
+        -- some cases.
+        local row, col = pos[1] - 1, pos[2] - 1
+
+        local node_type = vim.treesitter
+          .get_node({
+            pos = { row, col },
+          })
+          :type()
+
+        return not vim.tbl_contains(ignored_nodes, node_type)
+      end,
+    },
     fmt(
       [[
 if {} then
