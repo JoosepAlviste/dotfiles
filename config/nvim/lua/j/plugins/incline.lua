6f74c1fce709f374ee8cd123b613647b274be8d5
Joosep Alviste <joosep.alviste@gmail.com>
2023-04-23 10:15:06 +0300

    Vim: Do not show empty space in incline if no file icon

--- config/nvim/lua/j/plugins/incline.lua
+++ config/nvim/lua/j/plugins/incline.lua
@@ -11,10 +11,15 @@ require('incline').setup {
     local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
     local icon, color = require('nvim-web-devicons').get_icon_color(filename)
 
-    return {
-      { icon, guifg = color },
-      { ' ' },
-      { filename },
-    }
+    local segments = {}
+
+    if icon and #icon then
+      table.insert(segments, { icon, guifg = color })
+      table.insert(segments, { ' ' })
+    end
+
+    table.insert(segments, { filename })
+
+    return segments
   end,
 }
