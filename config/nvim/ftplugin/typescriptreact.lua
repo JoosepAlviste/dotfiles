d416439e95d378ce61e444aa6bcadd43b285a0bc
Joosep Alviste <joosep.alviste@gmail.com>
2023-05-10 08:37:01 +0300

    Vim: Add attribute quotes mini-snippet in TSX

--- config/nvim/ftplugin/typescriptreact.lua
+++ config/nvim/ftplugin/typescriptreact.lua
@@ -16,3 +16,20 @@ vim.keymap.set('i', '/', function()
 
   return '/'
 end, { expr = true, buffer = true })
+
+-- Inside an attribute: <button type| pressing = -> <button type="|"
+vim.keymap.set('i', '=', function()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local left_of_cursor_range = { cursor[1] - 1, cursor[2] - 1 }
+
+  -- The cursor location does not give us the correct node in this case, so we
+  -- need to get the node to the left of the cursor
+  local node = vim.treesitter.get_node { pos = left_of_cursor_range }
+  local parent_node = node:parent()
+
+  if node and parent_node and parent_node:type() == 'jsx_attribute' and node:type() == 'property_identifier' then
+    return '=""<left>'
+  end
+
+  return '='
+end, { expr = true, buffer = true })
