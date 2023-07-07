b7cf8d230ea543b55d9abda6fec933f093fcf257
Joosep Alviste <joosep.alviste@gmail.com>
2023-07-07 23:43:38 +0300

    Vim: Update to new Treesitter API

--- config/nvim/ftplugin/typescriptreact.lua
+++ config/nvim/ftplugin/typescriptreact.lua
@@ -1,8 +1,6 @@
 -- Automatically end a self-closing tag when pressing /
 vim.keymap.set('i', '/', function()
-  local ts_utils = require 'nvim-treesitter.ts_utils'
-
-  local node = ts_utils.get_node_at_cursor()
+  local node = vim.treesitter.get_node()
   if not node then
     return '/'
   end
@@ -15,21 +13,4 @@ vim.keymap.set('i', '/', function()
   end
 
   return '/'
-end, { expr = true, buffer = true })
-
--- Inside an attribute: <button type| pressing = -> <button type="|"
-vim.keymap.set('i', '=', function()
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local left_of_cursor_range = { cursor[1] - 1, cursor[2] - 1 }
-
-  -- The cursor location does not give us the correct node in this case, so we
-  -- need to get the node to the left of the cursor
-  local node = vim.treesitter.get_node { pos = left_of_cursor_range }
-  local parent_node = node:parent()
-
-  if node and parent_node and parent_node:type() == 'jsx_attribute' and node:type() == 'property_identifier' then
-    return '=""<left>'
-  end
-
-  return '='
 end, { expr = true, buffer = true })
