18b575709b0009ec2b6cc32b917d24514489887c
Joosep Alviste <joosep.alviste@gmail.com>
2023-07-07 23:37:55 +0300

    Vim: Improve cmp settings
    I found these somewhere and they seem to work nicely, I think.

--- config/nvim/lua/j/plugins/cmp.lua
+++ config/nvim/lua/j/plugins/cmp.lua
@@ -59,8 +59,8 @@ cmp.setup {
     disallow_fuzzy_matching = true,
     disallow_fullfuzzy_matching = true,
     disallow_partial_fuzzy_matching = true,
-    disallow_partial_matching = true,
-    disallow_prefix_unmatching = false,
+    disallow_partial_matching = false,
+    disallow_prefix_unmatching = true,
   },
 
   window = {
@@ -105,6 +105,33 @@ cmp.setup {
 
       return vim_item
     end,
+  },
+
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+
+      -- copied from cmp-under, but I don't think I need the plugin for this.
+      -- I might add some more of my own.
+      function(entry1, entry2)
+        local _, entry1_under = entry1.completion_item.label:find '^_+'
+        local _, entry2_under = entry2.completion_item.label:find '^_+'
+        entry1_under = entry1_under or 0
+        entry2_under = entry2_under or 0
+        if entry1_under > entry2_under then
+          return false
+        elseif entry1_under < entry2_under then
+          return true
+        end
+      end,
+
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
   },
 }
 
