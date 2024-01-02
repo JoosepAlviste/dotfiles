e5229267c4cd2f022fed1006005b011d23f7ca60
Joosep Alviste <joosep.alviste@gmail.com>
2024-01-02 08:16:57 +0200

    Vim: Add cwd detection for ESLint based on .eslintrc.json
    This helps it work a bit better in a monorepo.

--- config/nvim/lua/j/plugins/lint.lua
+++ config/nvim/lua/j/plugins/lint.lua
@@ -2,6 +2,8 @@ return {
   'mfussenegger/nvim-lint',
   ft = { 'vue', 'typescript', 'typescriptreact', 'javascript' },
   config = function()
+    local util = require 'lspconfig.util'
+
     require('lint').linters_by_ft = {
       vue = { 'eslint_d' },
       typescript = { 'eslint_d' },
@@ -10,8 +12,10 @@ return {
     }
 
     vim.api.nvim_create_autocmd({ 'BufWritePost', 'InsertLeave', 'BufEnter' }, {
-      callback = function()
-        require('lint').try_lint()
+      callback = function(args)
+        require('lint').try_lint(nil, {
+          cwd = util.root_pattern { '.eslintrc.json', 'package.json', '.git' }(args.file),
+        })
       end,
     })
   end,
