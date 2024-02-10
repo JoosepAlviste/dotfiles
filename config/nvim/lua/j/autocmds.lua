95f9689a46803b2be2e0cbe38a96fffd3ca86378
Joosep Alviste <joosep.alviste@gmail.com>
2024-02-10 09:17:05 +0200

    Vim: Restore cursor position when opening a file

--- config/nvim/lua/j/autocmds.lua
+++ config/nvim/lua/j/autocmds.lua
@@ -17,3 +17,14 @@ vim.api.nvim_create_autocmd('WinEnter', {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Open file at the last position it was edited earlier',
+  group = group,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, mark)
+    end
+  end,
+})
