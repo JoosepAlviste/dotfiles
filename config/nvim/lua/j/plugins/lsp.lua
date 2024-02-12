b25d6f83bec524126797481393401a409ee04dc0
Joosep Alviste <joosep.alviste@gmail.com>
2024-02-12 19:35:02 +0200

    Vim: Add better TypeScript errors plugin
    Let's see if I'll remember to use this.

--- config/nvim/lua/j/plugins/lsp.lua
+++ config/nvim/lua/j/plugins/lsp.lua
@@ -244,4 +244,14 @@ return {
       },
     },
   },
+  {
+    'OlegGulevskyy/better-ts-errors.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    config = {
+      keymaps = {
+        toggle = '<leader>zd', -- default '<leader>dd'
+        go_to_definition = '<leader>zx', -- default '<leader>dx'
+      },
+    },
+  },
 }
