aa5e9aa46e71ec8d9c62cf316f7549eb3a9f4beb
Joosep Alviste <joosep.alviste@gmail.com>
2024-05-02 09:15:05 +0300

    Vim: Update to Volar 2

--- config/nvim/lua/j/plugins/lsp/vue_ls.lua
+++ config/nvim/lua/j/plugins/lsp/vue_ls.lua
@@ -39,6 +39,12 @@ require('lspconfig').volar.setup {
     },
   },
 
+  init_options = {
+    vue = {
+      hybridMode = false,
+    },
+  },
+
   on_new_config = function(new_config, new_root_dir)
     new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
   end,
