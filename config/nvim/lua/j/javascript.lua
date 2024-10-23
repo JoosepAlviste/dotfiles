1947694df7dce023613fe3dbb029b6968527fdbe
Joosep Alviste <joosep.alviste@gmail.com>
2024-10-23 21:26:25 +0300

    Vim: Simplify async snippet

--- config/nvim/lua/j/javascript.lua
+++ config/nvim/lua/j/javascript.lua
@@ -7,17 +7,12 @@ function M.add_async()
   -- but "t" is not inserted because it's the trigger.
   vim.api.nvim_feedkeys('t', 'n', true)
 
-  local buffer = vim.fn.bufnr()
-
   local text_before_cursor = vim.fn.getline('.'):sub(vim.fn.col '.' - 4, vim.fn.col '.' - 1)
   if text_before_cursor ~= 'awai' then
     return
   end
 
-  local pos = vim.api.nvim_win_get_cursor(0)
-  -- Use one column to the left of the cursor to make sure we're on the `await` word
-  local row, col = pos[1] - 1, pos[2] - 1
-  local current_node = vim.treesitter.get_node { ignore_injections = false, pos = { row, col } }
+  local current_node = vim.treesitter.get_node { ignore_injections = false }
   local function_node = require('j.treesitter_utils').find_node_ancestor(
     { 'arrow_function', 'function_declaration', 'function' },
     current_node
@@ -32,7 +27,7 @@ function M.add_async()
   end
 
   local start_row, start_col = vim.treesitter.get_node_range(function_node)
-  vim.api.nvim_buf_set_text(buffer, start_row, start_col, start_row, start_col, { 'async ' })
+  vim.api.nvim_buf_set_text(0, start_row, start_col, start_row, start_col, { 'async ' })
 end
 
 return M
