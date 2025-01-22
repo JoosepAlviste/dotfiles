c3ef825ac64fbe47c4476028642c2e759c082392
Joosep Alviste <joosep.alviste@gmail.com>
2025-01-22 20:52:03 +0200

    Vim: Show project-local imports first in completion

--- config/nvim/lua/j/plugins/cmp.lua
+++ config/nvim/lua/j/plugins/cmp.lua
@@ -1,3 +1,52 @@
+---Show project-local variables first in the completion menu.
+---@param e1 cmp.Entry
+---@param e2 cmp.Entry
+---@return nil|boolean
+local function compare_project_local_first(e1, e2)
+  ---@param entry cmp.Entry
+  ---@return boolean
+  local function has_auto_import(entry)
+    local entry_names = entry.completion_item.data and entry.completion_item.data.entryNames
+
+    return entry_names and entry_names[1].data
+  end
+
+  ---@param entry cmp.Entry
+  ---@return boolean
+  local function is_local(entry)
+    local entry_names = entry.completion_item.data.entryNames
+    local auto_import_obj = entry_names and entry_names[1].data
+
+    if not auto_import_obj or not auto_import_obj.moduleSpecifier then
+      return false
+    end
+
+    return vim.startswith(auto_import_obj.moduleSpecifier, '.')
+  end
+
+  if not has_auto_import(e1) and not has_auto_import(e2) then
+    return nil
+  end
+
+  if not has_auto_import(e1) and has_auto_import(e2) then
+    return true
+  end
+
+  if has_auto_import(e1) and not has_auto_import(e2) then
+    return false
+  end
+
+  if is_local(e1) and not is_local(e2) then
+    return true
+  end
+
+  if not is_local(e1) and is_local(e2) then
+    return false
+  end
+
+  return nil
+end
+
 return {
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
@@ -41,7 +90,7 @@ return {
         disallow_fullfuzzy_matching = true,
         disallow_partial_fuzzy_matching = true,
         disallow_partial_matching = false,
-        disallow_prefix_unmatching = true,
+        disallow_prefix_unmatching = false,
       },
 
       window = {
@@ -52,11 +101,10 @@ return {
 
       sorting = {
         comparators = {
-          cmp.config.compare.offset,
           cmp.config.compare.exact,
+          compare_project_local_first,
+          cmp.config.compare.scopes,
           cmp.config.compare.score,
-          cmp.config.compare.kind,
-          cmp.config.compare.sort_text,
           cmp.config.compare.length,
           cmp.config.compare.order,
         },
