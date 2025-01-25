aebdad67cfd3578bc26ab1c821333cc395a26216
Joosep Alviste <joosep.alviste@gmail.com>
2025-01-25 22:48:51 +0200

    Vim: Enable context commentstring in Vue files

--- config/nvim/lua/j/plugins/editing.lua
+++ config/nvim/lua/j/plugins/editing.lua
@@ -12,7 +12,7 @@ return {
   },
   {
     'JoosepAlviste/nvim-ts-context-commentstring',
-    ft = { 'typescriptreact' },
+    ft = { 'typescriptreact', 'vue' },
     config = function()
       require('ts_context_commentstring').setup {
         enable_autocmd = false,
