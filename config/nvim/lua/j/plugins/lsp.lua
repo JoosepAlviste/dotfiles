ff41b3cc45b29c9d0ce1c4a3050c6d24b45d5b38
Joosep Alviste <joosep.alviste@gmail.com>
2025-08-23 20:13:41 +0300

    Vim: Configure LSP folding if available

--- config/nvim/lua/j/plugins/lsp.lua
+++ config/nvim/lua/j/plugins/lsp.lua
@@ -80,6 +80,11 @@ return {
               callback = vim.lsp.buf.clear_references,
             })
           end
+
+          if client:supports_method 'textDocument/foldingRange' then
+            local win = vim.api.nvim_get_current_win()
+            vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+          end
         end,
       })
 
