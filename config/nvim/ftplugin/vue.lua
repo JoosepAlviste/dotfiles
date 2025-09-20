213404e4ceb67aad46f5907eb2fb621958a30c6b
Joosep Alviste <joosep.alviste@gmail.com>
2025-09-20 14:13:50 +0300

    Vim: Fix auto-close tag mini-snippet

--- config/nvim/ftplugin/vue.lua
+++ config/nvim/ftplugin/vue.lua
@@ -17,20 +17,16 @@ vim.keymap.set('i', '/', function()
     return '/'
   end
 
-  local first_sibling_node = node:prev_named_sibling()
-  if not first_sibling_node then
+  local child_node = node:child(0)
+  if not child_node then
     return '/'
   end
 
-  local parent_node = node:parent()
-  local is_tag_writing_in_progress = node:type() == 'text' and parent_node:type() == 'element'
+  local is_writing_start_tag = node:type() == 'element' and child_node:type() == 'start_tag'
+  local start_tag_text = vim.treesitter.get_node_text(child_node, 0)
+  local is_tag_terminated = string.match(start_tag_text, '>$')
 
-  local is_start_tag = first_sibling_node:type() == 'start_tag'
-
-  local start_tag_text = vim.treesitter.get_node_text(first_sibling_node, 0)
-  local tag_is_already_terminated = string.match(start_tag_text, '>$')
-
-  if is_tag_writing_in_progress and is_start_tag and not tag_is_already_terminated then
+  if is_writing_start_tag and not is_tag_terminated then
     local char_at_cursor = vim.fn.strcharpart(vim.fn.strpart(vim.fn.getline '.', vim.fn.col '.' - 2), 0, 1)
     local already_have_space = char_at_cursor == ' '
 
