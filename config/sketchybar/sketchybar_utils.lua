2ac975c3aab9fbc5668004a344aa5dd4122569c8
Joosep Alviste <joosep.alviste@gmail.com>
2026-04-04 11:10:12 +0300

    feat(nvim): upgrade sketchybar

--- config/sketchybar/sketchybar_utils.lua
+++ config/sketchybar/sketchybar_utils.lua
@@ -36,19 +36,4 @@ function M.filter_table(callback, tbl)
   return result
 end
 
-function M.dump(o)
-  if type(o) == 'table' then
-    local s = '{ '
-    for k, v in pairs(o) do
-      if type(k) ~= 'number' then
-        k = '"' .. k .. '"'
-      end
-      s = s .. '[' .. k .. '] = ' .. M.dump(v) .. ','
-    end
-    return s .. '} '
-  else
-    return tostring(o)
-  end
-end
-
 return M
