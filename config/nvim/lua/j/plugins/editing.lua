bf7f01ab19353afb1a2d1717ab5b7c4c6ba268fd
Joosep Alviste <joosep.alviste@gmail.com>
2026-04-04 11:07:00 +0300

    feat(nvim): new leap.nvim url

--- config/nvim/lua/j/plugins/editing.lua
+++ config/nvim/lua/j/plugins/editing.lua
@@ -5,7 +5,7 @@ return {
     opts = {},
   },
   {
-    'ggandor/leap.nvim',
+    url = 'https://codeberg.org/andyg/leap.nvim',
     config = function()
       vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
       vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
