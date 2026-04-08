87720a7242afa54163aeb480b3f466e49c968b0a
Joosep Alviste <joosep.alviste@gmail.com>
2026-04-08 09:23:10 +0300

    fix(nvim): avoid errors in auto-session
    No need for this logic now, in the new auto-session and nvim version.

--- config/nvim/lua/j/plugins/auto_session.lua
+++ config/nvim/lua/j/plugins/auto_session.lua
@@ -1,14 +1,6 @@
-local close_all_floating_windows = require('j.utils').close_all_floating_windows
-
 return {
   'rmagatti/auto-session',
   opts = {
-    -- Resize Neovim after it is started, otherwise the cmdheight might be
-    -- super large when restoring the session
-    -- https://github.com/rmagatti/auto-session/issues/64
-    -- https://github.com/neovim/neovim/issues/11330
-    post_restore_cmds = { 'silent !kill -s SIGWINCH $PPID' },
-    pre_save_cmds = { close_all_floating_windows, 'cclose' },
     log_level = 'error',
     session_lens = {
       load_on_setup = false,
