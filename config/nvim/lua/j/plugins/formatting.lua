b112d70db6c414adbb9b150124d52375d49cd80f
Joosep Alviste <joosep.alviste@gmail.com>
2026-05-02 10:54:36 +0300

    feat(nvim): set up oxfmt for formatting

--- config/nvim/lua/j/plugins/formatting.lua
+++ config/nvim/lua/j/plugins/formatting.lua
@@ -1,25 +1,29 @@
 return {
   'stevearc/conform.nvim',
-  ft = { 'lua', 'vue', 'typescript', 'typescriptreact', 'javascript', 'json', 'jsonc' },
+  ft = { 'lua', 'vue', 'typescript', 'typescriptreact', 'javascript', 'json', 'jsonc', 'html', 'css' },
   opts = {
+    formatters = {
+      oxfmt = {
+        require_cwd = true,
+      },
+      prettier = {
+        require_cwd = true,
+      },
+    },
     formatters_by_ft = {
       lua = { 'stylua' },
       vue = { 'eslint_d' },
-      typescript = { 'eslint_d' },
+      typescript = { 'eslint_d', 'oxfmt' },
       typescriptreact = { 'eslint_d' },
       javascript = { 'eslint_d' },
-      json = { 'prettier' },
-      jsonc = { 'prettier' },
+      json = { 'prettier', 'oxfmt' },
+      jsonc = { 'prettier', 'oxfmt' },
+      html = { 'oxfmt' },
+      css = { 'oxfmt' },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      quiet = true,
     },
   },
-  config = function(_, opts)
-    require('conform').setup(opts)
-
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      pattern = '*',
-      callback = function(args)
-        require('conform').format { bufnr = args.buf }
-      end,
-    })
-  end,
 }
