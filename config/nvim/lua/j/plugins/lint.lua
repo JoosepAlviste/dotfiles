7183f1b9e0c0802123d8168ba6f4056e0a773837
Joosep Alviste <joosep.alviste@gmail.com>
2026-05-28 20:32:55 +0300

    feat(nvim): set up Stylelint

--- config/nvim/lua/j/plugins/lint.lua
+++ config/nvim/lua/j/plugins/lint.lua
@@ -2,17 +2,21 @@ return {
   'mfussenegger/nvim-lint',
   ft = { 'vue', 'typescript', 'typescriptreact', 'javascript' },
   config = function()
+    local stylelint = vim.fn.executable(require('lint').linters['stylelint'].cmd()) == 1 and 'stylelint' or nil
+
     require('lint').linters_by_ft = {
-      vue = { 'eslint_d' },
+      vue = { 'eslint_d', stylelint },
       typescript = { 'eslint_d' },
       typescriptreact = { 'eslint_d' },
       javascript = { 'eslint_d' },
+      css = { stylelint },
+      scss = { stylelint },
     }
 
     vim.api.nvim_create_autocmd({ 'BufWritePost', 'InsertLeave', 'BufEnter' }, {
       callback = function(args)
         require('lint').try_lint(nil, {
-          cwd = vim.fs.root(args.file, { '.eslintrc.json', 'package.json', '.git' }),
+          cwd = vim.fs.root(args.file, { '.eslintrc.json', '.git' }),
         })
       end,
     })
